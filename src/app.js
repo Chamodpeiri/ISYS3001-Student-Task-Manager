2642f08de0bf50d01fe4a80f7e885f2ae9b98115
Sachini De silva <sachinikavindya53@gmail.com>
2026-09-22 15:52:51 +0530

    chore(quality): add security and maintenance checks

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 const session = require("express-session");
 const { MongoStore } = require("connect-mongo");
 const authRoutes = require("./routes/auth");
@@ -6,6 +7,7 @@ const taskRoutes = require("./routes/tasks");
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
